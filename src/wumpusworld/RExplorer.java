e57401c0e6e9c017d6fce5c4df7c2879612ebf2c
freivalds1 <freivalds1@gmail.com>
2016-10-13 23:20:18 -0600

    Basic reactions
    The reactive explorer can chain moves, but his reliance on the senseCell() method is killing functionality.
    The line "if (world.senseCell(x,y)[0] && world.senseCell(x,y)[1] == false){" NEVER passes, and hence he always senses danger for unknown reasons.
    Despite what appear to be solid checks to prevent them, null-pointer crashes occur any time the agent attempts to move out of world bounds from a the feelBump() method call when it hits the perimeter walls. No idea why, it doesn't seem to do anything with a non-existent cell.
    Been troubleshooting for hours, caught a few other problems but haven't solved these two yet.

--- src/wumpusworld/RExplorer.java
+++ src/wumpusworld/RExplorer.java
@@ -1,18 +1,227 @@
-package wumpusworld;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+package wumpusworld;
+import java.util.Random;
 /**
  *
- * @author Lizzie Herman
+ * @author Lizzie Herman, Ryan Freivalds
+ * 
  */
 public class RExplorer extends Explorer {
 
-    public RExplorer(WumpusWorld w, int n, int num) {
+    public RExplorer(WumpusWorld w, int n, int num) { //n is the size of the world, num is the number of wumpuses
         super(w, n, num);
+        System.out.println("So it begins."); //testline
+        agressiveExplorer();
+        
+        
     }
 
+    public void cautiousExplorer(){  
+        Random rand = new Random();
+        
+        double num;
+        /* testlines
+        move();
+        move();
+        turnLeft();
+        move();
+        move();
+        */
+        while (world.senseCell(x,y)[2] != true){ //while we do not see glittering 
+            System.out.println("Current position is (" + x + "," + y + ")"); //testline
+            if (world.senseCell(x,y)[0] && world.senseCell(x,y)[1] == false){ //if there is no present danger, we are supposed to move at random. CURRENTLY BROKEN. He always sees danger.
+            System.out.println("No danger detected."); //testline
+                num = rand.nextDouble();    //generate a random number
+            /*
+            * rather than a 1/3rd chance to turn left, 1/3rd turn right, 1/3rd move forward
+            * we will do a 1/4 face-to-and-then-move North/South/East/West to cut down on sequences of turns
+            * that do not advance our progress. 
+            * Remember that the direction scheme is 1 = East, 2 = South, 3 = West, 4 = North.
+            */
+                if (num <= .25){  //move north
+                System.out.println("Moving North."); //testline
+                switch (direction) {
+                    case 1:
+                        //use the fewest moves to face North
+                        turnLeft();
+                        break;
+                    case 3:
+                        turnRight();
+                        break;
+                    case 2: //about face, if we are facing South
+                        turnRight();
+                        turnRight();
+                        break;
+                    default:
+                        break;
+                }
+                    move(); //and then move
+                }
+                
+                else if (num <= .50){ //move South
+                    System.out.println("Moving South."); //testline
+                    switch (direction) {
+                    case 3:
+                        //use the fewest moves to face South
+                        turnLeft();
+                        break;
+                    case 1:
+                        turnRight();
+                        break;
+                    case 4: //If we are facing North we about face
+                        turnRight();
+                        turnRight();
+                        break;
+                }
+                    move();
+                }
+                
+                else if (num <= .75){ //Move East
+                    System.out.println("Moving East."); //testline
+                    switch (direction) {
+                    case 2:
+                        //use the fewest moves to face East
+                        turnLeft();
+                        break;
+                    case 4:
+                        turnRight();
+                        break;
+                    case 3: //if we are facing West turn around
+                        turnRight();
+                        turnRight();
+                        break;
+                    default:
+                        break;
+                }
+                    move();
+                }
+                
+                else if (num <= 1){ //move West
+                    System.out.println("Moving West."); //testline
+                    switch (direction) {
+                    case 4:
+                        //use the fewest moves to face West
+                        turnLeft();
+                        break;
+                    case 2:
+                        turnRight();
+                        break;
+                    case 1: //about face, if we are facing East
+                        turnRight();
+                        turnRight();
+                        break;
+                    default:
+                        break;
+                }
+                    move();
+                }
+            
+            } //END of random move if current space is safe
+            else {
+                System.out.println("Danger is nearby.");
+                turnRight();
+                turnRight();
+                move();
+            }
+            
+        grabGold();
+        }
+    }
+    
+    public void agressiveExplorer(){
+        Random rand = new Random();
+        double num;
+        while (world.senseCell(x,y)[2] != true){ 
+        num = rand.nextDouble();
+        if (num <= .25){  //move north
+                System.out.println("Moving North."); //testline
+                switch (direction) {
+                    case 1:
+                        //use the fewest moves to face North
+                        turnLeft();
+                        break;
+                    case 3:
+                        turnRight();
+                        break;
+                    case 2: //about face, if we are facing South
+                        turnRight();
+                        turnRight();
+                        break;
+                    default:
+                        break;
+                }
+                    move(); //and then move
+                }
+                
+                else if (num <= .50){ //move South
+                    System.out.println("Moving South."); //testline
+                    switch (direction) {
+                    case 3:
+                        //use the fewest moves to face South
+                        turnLeft();
+                        break;
+                    case 1:
+                        turnRight();
+                        break;
+                    case 4: //If we are facing North we about face
+                        turnRight();
+                        turnRight();
+                        break;
+                }
+                    move();
+                }
+                
+                else if (num <= .75){ //Move East
+                    System.out.println("Moving East."); //testline
+                    switch (direction) {
+                    case 2:
+                        //use the fewest moves to face East
+                        turnLeft();
+                        break;
+                    case 4:
+                        turnRight();
+                        break;
+                    case 3: //if we are facing West turn around
+                        turnRight();
+                        turnRight();
+                        break;
+                    default:
+                        break;
+                }
+                    move();
+                }
+                
+                else if (num <= 1){ //move West
+                    System.out.println("Moving West."); //testline
+                    switch (direction) {
+                    case 4:
+                        //use the fewest moves to face West
+                        turnLeft();
+                        break;
+                    case 2:
+                        turnRight();
+                        break;
+                    case 1: //about face, if we are facing East
+                        turnRight();
+                        turnRight();
+                        break;
+                    default:
+                        break;
+                }
+                    move();
+                }
+        }
+        grabGold();
+    }
+    
     public void feelBump(int x1, int y1){
-        x = x1;
-        y = y1;
+        //x = x1;
+        //y = y1;
         System.out.print("Feel Bump");
         /*
          * TO-DO
@@ -31,7 +240,6 @@ public class RExplorer extends Explorer {
     
     // takes in last safe location and whetheror not a wumpus killed them
     public void die(int x1, int y1, boolean wumpus){
-        timesDied++;
         cost -= 1000;
         x = x1;
         y = y1;
@@ -67,13 +275,6 @@ public class RExplorer extends Explorer {
          * send sense info to knowledge base
          */
     }
-    
-    public void getPercepts(){
-        boolean[] senses = world.senseCell(x, y);
-        // if don't smell stench or feel breeze then surrounding cells are safe
-        if(!(senses[0] || senses[1])) System.out.print("Surrounding Cells are Safe");
-        /*
-         * where do we go
-        */
-    }
 }
+
+    
