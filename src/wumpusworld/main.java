e4ecc49d78da91b76681b6cf5c0dfa5e1c433846
LizzieHerman <lizziemn96@gmail.com>
2016-10-13 21:09:12 -0600

    you can't run the gui code without the picture files

--- src/wumpusworld/main.java
+++ src/wumpusworld/main.java
@@ -8,7 +8,7 @@ public class main {
     public static void main(String[] args) {
         for(int i = 5; i <= 25; i += 5){
             WumpusWorld world = new WumpusWorld(i);
-            int numWump = world.generateWorld(0.01, 0.01, 0.01);
+            int numWump = world.generateWorld(0.20, 0.20, 0.20);
             Explorer agent = new FOExplorer(world, i, numWump);
             world.setExplorer(agent);
             // sense the cell you are currently in
