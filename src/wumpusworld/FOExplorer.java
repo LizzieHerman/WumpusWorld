8d73fed743ae811162891ed8aacf33206c13de46
LizzieHerman <lizziemn96@gmail.com>
2016-10-17 14:37:10 -0600

    fixed RExplorer

--- src/wumpusworld/FOExplorer.java
+++ src/wumpusworld/FOExplorer.java
@@ -20,40 +20,10 @@ public class FOExplorer extends Explorer {
 //         for(int i = 0; i < numRules; i++){
 //        	 rules[i] = new Clause(i);
 //         }
-}
-    
-    public void move(){
-        cost -= 10;
-        switch(direction){
-            case 1: //moving East
-                x++;
-                break;
-            case 2: //Moving South
-                y--;
-                break;
-            case 3: //Moving West
-                x--;
-                break;
-            case 4: //Moving North
-                y++;
-                break;
-        }
-        world.moveExplorer(x, y);
-        getPercepts();
     }
     
-    public void turnRight(){
-        cost -= 10;
-        if(direction == 4) direction = 1; //if facing North and turning right, we are now facing East
-        else direction++; //otherwise our direction is properly set with ++
-        world.turnExplorer(false); //indicates to the world class that we are turning right
-    }
-    
-    public void turnLeft(){
-        cost -= 10;
-        if(direction == 1) direction = 4; //if facing East and turning Left, we are now facing North
-        else direction--; //otherwise our direction is properly set with --
-        world.turnExplorer(true); //indicates to the world class that we are turning left
+    public void start(){
+        move();
     }
     
     // return whether able to shoot arrow
@@ -65,6 +35,14 @@ public class FOExplorer extends Explorer {
         arrows--;
         world.shootArrow(x,y,direction); //sending our "starting" square of firing, and which direction the arrow is being fired.
         return true;
+    }
+    
+    public void feelBump(int x1, int y1){
+        if(x < worldMap.size() && y < worldMap.size() && x >= 0 && y >= 0){ // test to see if we hit wall or obstacle
+            worldMap.setCell(x, y, 'o', true);
+        }
+        x = x1;
+        y = y1;
     }
     
     public void grabGold(){
