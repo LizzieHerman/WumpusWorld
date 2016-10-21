1dd4bfad0f33b4b1f6a19ea2cce6401efec0a76a
LizzieHerman <lizziemn96@gmail.com>
2016-10-21 09:52:24 -0600

    gui done

--- src/wumpusworld/main.java
+++ src/wumpusworld/main.java
@@ -2,17 +2,22 @@ package wumpusworld;
 
 /**
  *
- * @author Lizzie Herman
+ * @author Lizzie Herman, Greg Neznanski
  */
 public class main {
     public static void main(String[] args) {
         for(int i = 5; i <= 5; i += 5){
             WumpusWorld world = new WumpusWorld(i);
-            int numWump = world.generateWorld(0.20, 0.20, 0.20);
-            Explorer agent = new RExplorer(world, i, numWump);
+            int numWump = world.generateWorld(0.2, 0.2, 0.2);
+            Explorer agent = new FOExplorer(world, i, numWump);
             world.setExplorer(agent);
-            // start the agent
-            agent.start();
+            
+            System.out.println("Starting game size: " + i);
+            while(!agent.checkWin()){
+            	agent.start();
+            	//world.board.updateUI();
+            	//System.out.println("looping");
+            }
         }
     }
 
