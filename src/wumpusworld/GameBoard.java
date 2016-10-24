c69bdb0151f05aab81a881d6bebc7acaae7f7474
Lizzie Herman <lizziemn96@gmail.com>
2016-10-24 13:34:35 -0600

    Add files via upload

--- src/wumpusworld/GameBoard.java
+++ src/wumpusworld/GameBoard.java
@@ -11,52 +11,146 @@ package wumpusworld;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.border.Border;
 
 public class GameBoard extends JFrame {
-    GridLayout boardLayout;
-    JPanel board;
-    int[] stat = {0, 0, 0, 0, 0, 0, 0, 0};
-    Component[][] grid;
+    private GridLayout boardLayout;
+    private int[] stat = {0, 0, 0, 0, 0, 0, 0, 0}; // cost, arrow, move, death, stench, breeze, glimmer, bump
+    private Component[][] grid;
         
-    Container explorer1, explorer2, explorer3, explorer4, arrow1, arrow2, arrow3, arrow4, 
-            wumpus, deadwumpus, obstacle, pit, gold, inGold, inWumpus, inPit, empty;
-    
-    public GameBoard(int wump, String[][] world, Cell[][] actualWorld){
+    public GameBoard(int wump, String[][] world){
         // the board will include an indexing of the board spaces
+        setMaximumSize(new Dimension(1050,1050));
         boardLayout = new GridLayout(world.length+1, world.length+1);
         stat[1] = wump;
         grid = new Container[world.length+1][world.length+1];
-        makeGrid(world, actualWorld);
+        makeGrid(world);
         initPanels();
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setTitle("Game Board");
     }
     
-    private void makeGrid(String[][] world, Cell[][] actualWorld){
+    private void makeGrid(String[][] world){
+        int size = 100;
+        if(grid.length <= 9) size = 100;
+        else if(grid.length <= 10) size = 90;
+        else if(grid.length <= 11) size = 80;
+        else if(grid.length <= 12) size = 75;
+        else if(grid.length <= 15) size = 60;
+        else if(grid.length <= 18) size = 50;
+        else if(grid.length <= 22) size = 40;
+        else size = 36;
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid.length; j++){
                 int x = i-1; int y = j-1;
                 if(i == 0 && j == 0){
-                    grid[i][j] = Box.createRigidArea(new Dimension(50,50));
+                    grid[i][j] = Box.createRigidArea(new Dimension(size,size));
                 }else if(i == 0 ){
                     grid[i][j] = new JLabel(Integer.toString(y));
+                    grid[i][j].setFont(new java.awt.Font("Monospaced", 0, size));
                 }else if(j == 0){
                     grid[i][j] = new JLabel(Integer.toString(x));
+                    grid[i][j].setFont(new java.awt.Font("Monospaced", 0, size));
                 }else{
                     String inCell = world[x][y];
-                    
-                    if(i <= actualWorld.length && j <= actualWorld.length){
-                    	grid[i][j] = new JLabel(actualWorld[i-1][j-1].getDisplay());
+                    ImageIcon imgicon;
+                    if(inCell.isEmpty() || inCell.equalsIgnoreCase("empty")){
+                        imgicon = new ImageIcon(getClass().getResource("/wumpusworld/empty.png"));
+                    }else if(inCell.equalsIgnoreCase("explorer")){
+                        imgicon = new ImageIcon(getClass().getResource("/wumpusworld/explorer1.png"));
+                    }else if(inCell.equalsIgnoreCase("wumpus")){
+                        imgicon = new ImageIcon(getClass().getResource("/wumpusworld/wumpus.png"));
+                    }else if(inCell.equalsIgnoreCase("pit")){
+                        imgicon = new ImageIcon(getClass().getResource("/wumpusworld/pit.png"));
+                    }else if(inCell.equalsIgnoreCase("gold")){
+                        imgicon = new ImageIcon(getClass().getResource("/wumpusworld/gold.png"));
+                    }else if(inCell.equalsIgnoreCase("wall")){
+                        imgicon = new ImageIcon(getClass().getResource("/wumpusworld/wall.png"));
+                    }else{
+                        imgicon = new ImageIcon(getClass().getResource("/wumpusworld/empty.png"));
                     }
+                    Image img = imgicon.getImage(); 
+                    Image newimg = img.getScaledInstance(size, size,  java.awt.Image.SCALE_SMOOTH);  
+                    imgicon = new ImageIcon(newimg);
+                    grid[i][j] = new JLabel(imgicon);
                 }
             }
         }  
     }
     
+    public void changeGrid(int x, int y, String inCell, int n){
+        int size = 100;
+        if(grid.length <= 9) size = 100;
+        else if(grid.length <= 10) size = 90;
+        else if(grid.length <= 11) size = 80;
+        else if(grid.length <= 12) size = 75;
+        else if(grid.length <= 15) size = 60;
+        else if(grid.length <= 18) size = 50;
+        else if(grid.length <= 22) size = 40;
+        else size = 36;
+        x++; y++;
+        ImageIcon imgicon;
+        if(inCell.equalsIgnoreCase("empty")){
+            imgicon = new ImageIcon(getClass().getResource("/wumpusworld/empty.png"));
+        }else if(inCell.equalsIgnoreCase("explorer") && n == 1){
+            imgicon = new ImageIcon(getClass().getResource("/wumpusworld/explorer1.png"));
+        }else if(inCell.equalsIgnoreCase("explorer") && n == 2){
+            imgicon = new ImageIcon(getClass().getResource("/wumpusworld/explorer2.png"));
+        }else if(inCell.equalsIgnoreCase("explorer") && n == 3){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/explorer3.png")));
+        }else if(inCell.equalsIgnoreCase("explorer") && n == 4){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/explorer4.png")));
+        }else if(inCell.equalsIgnoreCase("wumpus")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/wumpus.png")));
+        }else if(inCell.equalsIgnoreCase("deadwumpus")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/deadwumpus.png")));
+        }else if(inCell.equalsIgnoreCase("inwumpus")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/inwumpus.png")));
+        }else if(inCell.equalsIgnoreCase("pit")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/pit.png")));
+        }else if(inCell.equalsIgnoreCase("inpit")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/inpit.png")));
+        }else if(inCell.equalsIgnoreCase("gold")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/gold.png")));
+        }else if(inCell.equalsIgnoreCase("ingold")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/ingold.png")));
+        }else if(inCell.equalsIgnoreCase("wall")){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/wall.png")));
+        }else if(inCell.equalsIgnoreCase("path") && n == 1){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/right.png")));
+        }else if(inCell.equalsIgnoreCase("path") && n == 2){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/down.png")));
+        }else if(inCell.equalsIgnoreCase("path") && n == 3){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/left.png")));
+        }else if(inCell.equalsIgnoreCase("path") && n == 4){
+            imgicon = (new ImageIcon(getClass().getResource("/wumpusworld/up.png")));
+        }else{
+            imgicon = new ImageIcon(getClass().getResource("/wumpusworld/empty.png"));
+        }
+        Image img = imgicon.getImage(); 
+        Image newimg = img.getScaledInstance(size, size,  java.awt.Image.SCALE_SMOOTH);  
+        imgicon = new ImageIcon(newimg);
+        grid[x][y] = new JLabel(imgicon);
+    }
+    
+    public void changeCost(int cost){
+        stat[0] = cost;
+    }
+    
+    public void changeBoolStates(int state, boolean data){
+        stat[state] = data ? 1 : 0;
+    }
+    
+    public void incremStates(int state){
+        stat[state] += 1;
+    }
+    
+    public void decremStates(int state){
+        stat[state] -= 1;
+    }
+    
     private void initPanels(){
         //make board
-        board = new JPanel();
+        JPanel board = new JPanel();
         board.setLayout(boardLayout);
         for(int j = grid.length-1; j >= 0; j--){
             for(int i = 0; i < grid.length; i++){
@@ -90,12 +184,9 @@ public class GameBoard extends JFrame {
         line2.add(glimmer);
         line2.add(bump);
         // combine line one, two and the button
-        next.setAlignmentX(CENTER_ALIGNMENT);
         stats.add(line1);
         stats.add(Box.createRigidArea(new Dimension(0,10)));
         stats.add(line2);
-        stats.add(Box.createRigidArea(new Dimension(0,10)));
-        stats.add(next);
         stats.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
         
         // combine board and stats
@@ -104,12 +195,25 @@ public class GameBoard extends JFrame {
         this.add(stats, BorderLayout.SOUTH);
     }
     
-    public void updateUI(){
-    	board.revalidate();
-    	board.repaint();
+    public void updatePanels(){
+        JPanel board = new JPanel();
+        board.setLayout(boardLayout);
+        for(int j = grid.length-1; j >= 0; j--){
+            for(int i = 0; i < grid.length; i++){
+                board.add(grid[i][j]);
+            }
+        }
+        cost.setText("Cost: " + stat[0]);
+        arrow.setText("Arrows Left: " + stat[1]);
+        move.setText("Moves: " + stat[2]);
+        death.setText("Death Count: " + stat[3]);
+        stench.setText("Smell Stench: " + stat[4]);
+        breeze.setText("Feel Breeze: " + stat[5]);
+        glimmer.setText("See Glimmer: " + stat[6]);
+        bump.setText("Bump Count: " + stat[7]);
+        this.add(board, BorderLayout.NORTH);
     }
     
-    private JButton next = new JButton("Next Move");
     private JLabel cost = new JLabel();
     private JLabel arrow = new JLabel();
     private JLabel move = new JLabel();
